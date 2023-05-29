960af02aeea060d73e7bc955c7ce3ad1d0509c3c
yeeyeeeyee <aagg50211@gmail.com>
2023-05-30 01:01:54 +0800

    set plus 如何傳遞數值

--- app/src/main/java/com/example/basic_calculator/ui.kt
+++ app/src/main/java/com/example/basic_calculator/ui.kt
@@ -37,38 +37,35 @@ fun CalculatorUi() {
         //位置
         Column(verticalArrangement = Arrangement.Bottom) {
             //顯示已輸入的數字
-            Row(Modifier.fillMaxWidth()) {
-                //數字1: 運算符號前
-                Text(
-                    text = number.num1Value,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(),
-                    color = Color.Gray,
-                    fontSize = 30.sp,
-                )
-                //雲算符號
-                Text(
-                    text = number.operation,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(),
-                    color = Color.Gray,
-                    fontSize = 30.sp,
-                )
-                //數字2: 運算符號後
-                Text(
-                    text = number.num2Value,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(),
-                    color = Color.Gray,
-                    fontSize = 30.sp,
-                )
-            }
+
+            //數字1: 運算符號前
+            Text(
+                text = number.num1Value,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color.Gray,
+                fontSize = 30.sp,
+            )
+            //雲算符號
+            Text(
+                text = number.operation,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color.Gray,
+                fontSize = 30.sp,
+            )
+            //數字2: 運算符號後
+            Text(
+                text = number.num2Value,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = Color.Gray,
+                fontSize = 30.sp,
+            )
+
             //顯示輸入文字
             Text(
                 text = display,
@@ -92,7 +89,7 @@ fun CalculatorUi() {
                 ButtonStyle("%") {
                     if (number.operation == "") {
 
-                    } else{
+                    } else {
 
                     }
                 }
@@ -161,8 +158,10 @@ fun CalculatorUi() {
                 ButtonStyle("2", onclick = { display += "2" })
                 ButtonStyle("3", onclick = { display += "3" })
                 //如果display 沒有數字無法啟用,當有數字時同時設定number.value的值跟operation為"+"
-                ButtonStyle("+"){
-                    if (display!="="){
+                ButtonStyle("+") {
+                    if (display != "=") {
+                        number.num1Value = display.substring(1)
+                        number.operation = "+"
 
                     }
                 }
@@ -177,10 +176,10 @@ fun CalculatorUi() {
                 ButtonStyle("+/-", onclick = {})
                 ButtonStyle("0", onclick = { display += "0" })
                 //確認時否有小數點沒有的話就加跟把長度記下來,長度小於小數點的位置則可以使用 反之亦然
-                ButtonStyle("."){
+                ButtonStyle(".") {
                 }
                 //需要先把文字轉成數字才能運算
-                ButtonStyle("="){
+                ButtonStyle("=") {
 
                 }
             }
